b7e895eafc128730b0fc862f6cb8e04fa86de9ed
david marcais <dmarcais@yahoo.com>
2021-01-18 10:12:54 +0100

    add principal function in script.js

---
+++ Js/script.js
@@ -0,0 +1,32 @@
+function NewGame() {
+  return
+}
+
+function displayScoreGobal() {
+  return
+}
+
+function displayRound() {
+  return
+}
+
+function displayDice() {
+  return
+}
+
+function displayWhoIsPlaying() {
+  return
+}
+
+function Hold() {
+  return
+}
+
+function RollDice() {
+  return
+}
+
+
+
+
+
